18382e62ded37764c015881d870b7b7f5efab90d
IrinaBystrova <dylan_@mail.ru>
2019-02-06 17:49:45 +0300

    series_spider parses any quantity of items new_items_spider works till last episode_date

--- popcorn/spiders/new_items.py
+++ popcorn/spiders/new_items.py
@@ -1,21 +1,20 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
-from datetime import datetime, timedelta
+from datetime import datetime
 from typing import List
 from popcorn.items import NewItemsItem
+from sqlalchemy import create_engine
 
 
 class LostfilmNewSpider(CrawlSpider):
     name = 'new_items_spider'
     allowed_dominas = ['www.lostfilm.tv']
     start_urls = ['https://www.lostfilm.tv/new/page_1']
-    last_page = None
-
-    # правила перехода по страницам
     rules = [Rule(LinkExtractor(
         allow=(r'/new/page_\d{,2}\b',)),
         follow=True, callback='parse_page', process_links='finish_module'), ]
+    last_page = None
 
     def parse_page(self, response):
         my_selector = Selector(response)
@@ -23,29 +22,43 @@ class LostfilmNewSpider(CrawlSpider):
         item = NewItemsItem()
 
         for info in info4search:
-            # названия сериалов в одном списке
             series_name = info.xpath(
                 '//div[@class="name-ru"]/text()').extract()
-            # название эпизода и дата выхода эпизода в одном списке
             episode_info = info.xpath(
                 '//div[@class="alpha"]/text()').extract()
-
-        # название эпизода и дата выхода в отдельных списках
         episode_name, episode_date = [], []
         for i in range(len(series_name)):
             episode_name.append(episode_info[i + i])
             preparation_date = episode_info[i + i + 1]
             episode_date.append(preparation_date[-10:])
 
-        stop_time = datetime.now() - timedelta(7)
-        for j in range(len(series_name)):
-            date = episode_date[j]
-            if datetime.strptime(date, '%d.%m.%Y') > stop_time:
+        engine = create_engine(
+            'postgres://irinabystrova@localhost:5432/scrapy')
+        all_tables = engine.execute(
+            'select * from new_items')
+        if all_tables.rowcount == 0:
+            for j in range(len(series_name)):
                 item['series_name'] = series_name[0 + j]
                 item['episode_name'] = episode_name[0 + j]
                 item['episode_date'] = datetime.strptime(
                     (episode_date[0 + j]), '%d.%m.%Y')
                 yield item
+
+        else:
+            last_episode_date_object = engine.execute(
+                'select * from new_items where episode_date = '
+                '(select max(episode_date) from new_items)'
+                'order by id limit 1')
+            for i in last_episode_date_object:
+                last_episode_date = i[3]
+                for j in range(len(series_name)):
+                    if last_episode_date < datetime.strptime(
+                            (episode_date[0 + j]), '%d.%m.%Y'):
+                        item['series_name'] = series_name[0 + j]
+                        item['episode_name'] = episode_name[0 + j]
+                        item['episode_date'] = datetime.strptime(
+                            (episode_date[0 + j]), '%d.%m.%Y')
+                        yield item
 
         if len(item) == 0:
             self.last_page = int(response.meta['link_text'])
