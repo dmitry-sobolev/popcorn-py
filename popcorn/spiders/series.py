c103dc21fad046dbd8104b0a6735afbb14e22d29
IrinaBystrova <dylan_@mail.ru>
2019-02-06 13:49:40 +0300

    series_spider parses any quantity of items

--- popcorn/spiders/series.py
+++ popcorn/spiders/series.py
@@ -9,14 +9,14 @@ class LostfilmSeriesSpider(scrapy.Spider):
     allowed_dominas = ['www.lostfilm.tv']
     start_urls = [
         'https://www.lostfilm.tv/ajaxik.php?act=serial&type=search&o=0']
+    counter = 0
+    stop_signal = False
 
     def parse(self, response):
-        stop_num = 0
-        while stop_num <= 330:
+        while self.stop_signal is False:
             next_urls = (
              'https://www.lostfilm.tv/ajaxik.php?act=serial&type=search&o={}'
-            ).format(stop_num)
-            stop_num += 10
+            ).format(self.counter)
             yield response.follow(next_urls, callback=self.parse_data)
 
     def parse_data(self, response):
@@ -29,3 +29,8 @@ class LostfilmSeriesSpider(scrapy.Spider):
                 date=info['date'],
                 genres=info['genres']
             )
+
+        if len(results['data']) == 0:
+            self.stop_signal = True
+        else:
+            self.counter += 10
